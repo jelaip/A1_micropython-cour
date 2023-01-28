d45ad8e07522731e1a2ebcddb57f564ddf9cd4f5
jelaip <37291886+jelaip@users.noreply.github.com>
2023-01-28 13:07:47 +0100

    Create Exemple_8_Get.py

---
+++ Exemple_8_Get.py
@@ -0,0 +1,23 @@
+import network   #import des fonction lier au wifi
+import urequests	#import des fonction lier au requetes http
+import utime	#import des fonction lier au temps
+import ujson	#import des fonction lier aà la convertion en Json
+
+wlan = network.WLAN(network.STA_IF) # met la raspi en mode client wifi
+wlan.active(True) # active le mode client wifi
+
+ssid = ''
+password = ''
+wlan.connect(ssid, password) # connecte la raspi au réseau
+url = ""
+
+while(True):
+    try:
+        print("GET")
+        r = urequests.get(url) # lance une requete sur l'url
+        print(r.json()) # traite sa reponse en Json
+        r.close() # ferme la demande
+        utime.sleep(1)  
+    except Exception as e:
+        print(e)
+    
