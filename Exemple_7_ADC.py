81343b0e37cbd114453fa06a272f0eb68d475753
jelaip <37291886+jelaip@users.noreply.github.com>
2023-03-15 09:53:57 +0100

    Update Exemple_7_ADC.py

--- Exemple_7_ADC.py
+++ Exemple_7_ADC.py
@@ -5,6 +5,6 @@ adc = ADC(Pin(26, mode=Pin.IN)) # on prescise au programme que la pin 26 est une
 
 while True:# boucle infini
     val = adc.read_u16() # elle lit la valeur converti entre 0 et 65535 
-    val = val * (3.3 / 65535) # produit en crois pour retrouver le voltage
-    print(round(val, 2), "V") # ecrire la valeur de val en arrondisant à deux chiffres apres la virgule
+    val = val * (10 / 65535) # produit en crois pour retrouver le voltage
+    print(int(val)) # ecrire la valeur de val en arrondisant à l'entier
     time.sleep_ms(100) # attendre 100ms => 0.1 seconde
