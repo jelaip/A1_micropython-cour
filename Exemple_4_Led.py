085c6bf6500615001925ae1d260fae0925812f83
jelaip <37291886+jelaip@users.noreply.github.com>
2023-03-21 10:43:04 +0100

    Update Exemple_4_Led.py

--- Exemple_4_Led.py
+++ Exemple_4_Led.py
@@ -1,9 +1,9 @@
-from machine import Pin # importe dans le code la lib qui permet de gerer les Pin de sortie
+from machine import Pin # importe dans le code la lib qui permet de gerer les Pins de sortie
 import utime # importe dans le code la lib qui permet de gerer le temps
 
 pinNumber = 17 # declaration d'une variable pinNumber à 17
 led = Pin(pinNumber, mode=Pin.OUT) # declaration d'une variable de type pin ici la 17 
-                                   #et on prescise que c'est une pine de sortie de courant (OUT)
+                                   #et on prescise que c'est une pin de sortie de courant (OUT)
 
 while True:          # boucle infini
     led.toggle()     # change l'etat de la led
