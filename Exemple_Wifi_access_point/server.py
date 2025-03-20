aa2c923b132c82c56cbdd76f10c2d779f6ba1a52
jelaip <37291886+jelaip@users.noreply.github.com>
2025-03-20 14:42:24 +0100

    Update server.py

--- Exemple_Wifi_access_point/server.py
+++ Exemple_Wifi_access_point/server.py
@@ -6,30 +6,30 @@ ap = access_point("raspi_ap", "123456789")  # ssid && mdp
 ip = ap.ifconfig()[0]  
 print(ip)  # debug ip address (to connect to the server)
 
-page = open("index.html","r")
-indexhtml= page.read()
-page.close()
+page = open("index.html","r") # ouvre le fichier index.html (r = read)
+indexhtml= page.read() # stock le contenu du fichier dans la variable indexhtml
+page.close()  # ferme le fichier
 
 
-led = machine.Pin(14, machine.Pin.OUT)
+led = machine.Pin(14, machine.Pin.OUT) 
 
-@server.route("/", methods=["GET"])
-def home(request):
-    return str(indexhtml)
+@server.route("/", methods=["GET"]) # route de la page d'accueil, c'est un décorateur qui permet de lier une fonction à une route
+def home(request): # fonction de la page d'accueil
+    return str(indexhtml) # retourne le contenu de la variable indexhtml
 
-@server.route("/led/<param>", methods=["GET"])
+@server.route("/led/<param>", methods=["GET"]) # route de la led avec un parametre ex : /led/on ou /led/off
 def command(request, param):
-    if param == "on":
-        led.on()
+    if param == "on":   # si le parametre est "on"
+        led.on() # allume la led
         print("allume")
-        return json.dumps({"status": "on"}) 
-    led.off()
-    return json.dumps({"status": "off"})
+        return json.dumps({"status": "on"}) # retourne un json {"status": "on"} (return close la fonction)
+    led.off() # sinon eteint la led
+    return json.dumps({"status": "off"}) # retourne un json {"status": "off"}
 
-@server.catchall()
-def catchall(request):
-  return "Not found", 404
+@server.catchall() # route par defaut
+def catchall(request): 
+  return "Not found", 404 # retourne "Not found" et le code 404
 
-server.run()
+server.run() # lance le serveur
 
 
